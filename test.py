3c7ae06f7f1a94d79630e8bacdc40e1eb237c580
Daniil Volodin <volodin.daniil@rambler.ru>
2020-09-25 17:53:53 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
+from math import sqrt
+
+
 def even(a):
     return a % 2 == 0
 
 
+def square(a):
+    return [4 * a, a * a, a * sqrt(2)]
+
+
 x = int(input())
-print(even(x))
+print(even(x), square(x))
