bfeb07164025d81a6e7055716f541e0f8ca663f5
Daniil Volodin <volodin.daniil@rambler.ru>
2020-10-02 11:28:29 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,29 @@
 from math import sqrt
+from math import hypot
+
+
+class Rectangle:
+    def __init__(self, list_of_dots):
+        dot1 = list_of_dots[0]
+        dot2 = list_of_dots[1]
+        dot3 = list_of_dots[2]
+        dot4 = list_of_dots[3]
+        minx = min(dot1[0], dot2[0], dot3[0], dot4[0])
+        maxx = max(dot1[0], dot2[0], dot3[0], dot4[0])
+        miny = min(dot1[1], dot2[1], dot3[1], dot4[1])
+        maxy = max(dot1[1], dot2[1], dot3[1], dot4[1])
+        self.dot_1 = [minx, miny]
+        self.dot_2 = [minx, maxy]
+        self.dot_3 = [maxx, maxy]
+        self.dot_2 = [maxx, miny]
+        self.a = maxx - minx
+        self.b = maxy - miny
+
+    def perimeter(self):
+        return 2 * (self.a + self.b)
+
+    def area(self):
+        return self.a * self.b
 
 
 def even(a):
@@ -22,8 +47,11 @@ def repeat(a):
     return False
 
 
-lst = input().split()
+lst = list()
 
+for i in range(4):
+    dot = [int(a) for a in input().split()]
+    lst.append(dot)
 
-print(first_last(lst))
-print(repeat(lst))
+rec = Rectangle(lst)
+print(rec.perimeter(), rec.area())
