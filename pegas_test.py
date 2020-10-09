433952ebfb7d1a9e7162783c0d1093cfaf36ab8e
Daniil Volodin <volodin.daniil@rambler.ru>
2020-10-09 19:25:44 +0300

    Update pegas_test.py

--- pegas_test.py
+++ pegas_test.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -10,29 +11,75 @@ correct_password = 'rg#P5hZm4F'
 pegas_url = 'http://pegasus-edu.pegasus.ponyex.local/'
 
 
-def test_case_1():
+def pony_driver_init(driver_path):
     driver = webdriver.Safari()
     driver.get(pegas_url)
-    #assert driver.title == 'Пегас'
-    if driver.title == 'Пегас': print('ok')
-    else: print('not ok')
+    try:
+        title = WebDriverWait(driver, 10).until(
+                EC.title_is('Пегас'))
+    except:
+        print('driver not ok')
+        driver.close()
+        return 0
+    return driver
 
-    element_login = driver.find_element_by_name('login')
-    element_password = driver.find_element_by_name('password')
-    enter_button = driver.find_element_by_class_name('css-1hnkt5t')
 
-    element_login.send_keys(correct_login)
+def log_in(driver_path):
+    print('Step 1:', end=' ')
+    try:
+        driver = pony_driver_init(driver_path)
+        if driver == 0:
+            return 0
+
+        element_login = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.NAME, 'login')))
+
+        element_password = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.NAME, 'password')))
+
+        enter_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'css-1hnkt5t')))
+    except:
+        print('not ok')
+        driver.close()
+        return 0
+    else:
+        print('ok')
+
+    print('Step 2:', end=' ')
+    element_login.send_keys('log')
     element_password.send_keys(correct_password)
     enter_button.click()
+    try:
+        main_page_heading = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/section/section[2]/section/h1')))
+        if main_page_heading.text != 'Главная страница':
+            raise
 
-    #страница не успевает прогрузиться
+        menu_button = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, 'bp3-intent-success')))
 
-    #assert driver.find_element_by_xpath('//*[@id="root"]/section/section[2]/section/h1').text == 'Главная страница'
-    if driver.find_element_by_xpath('//*[@id="root"]/section/section[2]/section/h1').text == 'Главная страница': print('ok')
-    else: print('not ok')
+    except:
+        print('not ok')
+        driver.close()
+        return 0
+    else:
+        print('ok')
+
+    return driver
+
+
+def test_case_1(driver_path):
+    driver = log_in(driver_path)
+    time.sleep(10)
+    if driver == 0:
+        return
+    driver.close()
 
 
 #def test_case_2():
 
 
-test_case_1()
+if __name__ == "__main__":
+    driver_path = ''
+    test_case_1(driver_path)
