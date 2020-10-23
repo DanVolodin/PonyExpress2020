d61c7838677902389574623d4e076896f4d90d22
Daniil Volodin <volodin.daniil@rambler.ru>
2020-10-23 19:05:50 +0300

    Create test_case_5.py

---
+++ test_case_5.py
@@ -0,0 +1,59 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import login
+import paths as pth
+import functions as f
+from functions import ok
+from functions import not_ok
+from functions import step
+
+
+def test_():
+    f.test_num(5)
+
+    driver = login.log_in()
+    if f.driver_off(driver):
+        return
+
+    step(3)
+    try:
+        driver = f.click_menu_button(driver)
+        if f.driver_off(driver):
+            raise
+
+        driver = f.click_service_button(driver)
+        if f.driver_off(driver):
+            raise
+
+        driver = f.click_hosting_management_button(driver)
+        if f.driver_off(driver):
+            raise
+    except:
+        not_ok()
+        return
+    else:
+        ok()
+
+    step(4)
+    try:
+        driver = f.click_editing_user_groups_button(driver)
+        if f.driver_off(driver):
+            raise
+    except:
+        f.not_ok()
+        return
+    else:
+        ok()
+
+    time.sleep(5)
+    driver.close()
+
+
+if __name__ == "__main__":
+    logfile = open('logfile.txt', 'w')
+    test_()
