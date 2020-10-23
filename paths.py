7e954b74e926d3c4f022ca049318d2347e8e4488
Daniil Volodin <volodin.daniil@rambler.ru>
2020-10-23 19:06:36 +0300

    Create paths.py

---
+++ paths.py
@@ -0,0 +1,11 @@
+browser = 'Safari'
+driver_path = ''
+pegas_url = 'http://pegasus-edu.pegasus.ponyex.local/'
+
+correct_login = 'ext.mgu_education'
+correct_password = 'rg#P5hZm4F'
+
+incorrect_login = 'abacaba'
+incorrect_password = '0000'
+
+random_name = 'fgnmkc123'
