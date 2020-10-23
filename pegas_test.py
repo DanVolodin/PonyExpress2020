03f5af1360a4711094fe904bf4a3ec75ffcd94bb
Daniil Volodin <volodin.daniil@rambler.ru>
2020-10-23 19:07:10 +0300

    Update pegas_test.py

--- pegas_test.py
+++ pegas_test.py
@@ -2,7 +2,6 @@ import pytest
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
