a2b3c6d0a65a4f9347efad99fe49935752ef242c
Daniil Volodin <volodin.daniil@rambler.ru>
2020-10-28 00:44:16 +0300

    Create test_case_9.py

---
+++ test_case_9.py
@@ -0,0 +1,36 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import login
+import paths as pth
+import functions as f
+from functions import ok
+from functions import not_ok
+from functions import step
+
+
+def test_():
+    f.test_num(9)
+
+    driver = login.log_in()
+    if f.driver_off(driver):
+        return
+
+    try:
+        driver = f.open_71_arrival_without_sort_page(driver)
+        if f.driver_off(driver):
+            raise
+    except:
+        return
+
+    time.sleep(5)
+    driver.close()
+
+
+if __name__ == "__main__":
+    logfile = open('logfile.txt', 'w')
+    test_()
