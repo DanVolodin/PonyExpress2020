eb766e02326bbee043e02d70dbbd320093cc6df1
Daniil Volodin <volodin.daniil@rambler.ru>
2020-10-28 23:40:41 +0300

    Update test_case_1.py

--- test_case_1.py
+++ test_case_1.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 import login
 import paths as pth
+from paths import wait_time as wt
 import functions as f
 from functions import ok
 from functions import not_ok
@@ -18,7 +19,7 @@ def test_():
 
     driver = login.log_in()
 
-    time.sleep(5)
+    time.sleep(wt)
     if f.driver_off(driver):
         return
     driver.close()
