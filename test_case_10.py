af60eed63b38558ff1a65dc543f1c207ab51c58d
Daniil Volodin <volodin.daniil@rambler.ru>
2020-10-28 16:35:16 +0300

    Update test_case_10.py

--- test_case_10.py
+++ test_case_10.py
@@ -14,7 +14,7 @@ from functions import step
 
 
 def test_():
-    f.test_num(10)
+    f.test_num(9)
 
     driver = login.log_in()
     if f.driver_off(driver):
@@ -26,6 +26,27 @@ def test_():
             raise
     except:
         return
+
+    step(5)
+    try:
+        element_block_num = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH,
+            '//*[@id="root"]/section/section[2]/section/section/div[2]/div[3]/div[2]/div/div/div[3]/form/input')))
+
+        element_block_num.send_keys(pth.incorrect_block_num)
+        element_block_num.send_keys('\n')
+
+        element_warning = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div/div[1]/span')))
+
+        if element_warning.text != 'Parameter HostName can not be empty':
+            raise
+    except:
+        not_ok()
+        driver.close()
+        return
+    else:
+        ok()
 
     time.sleep(5)
     driver.close()
