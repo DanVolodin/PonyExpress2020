416620e4207668c8f72c4f368652ea78ac92d269
Daniil Volodin <volodin.daniil@rambler.ru>
2020-11-13 16:46:00 +0300

    Update pegas_requests_functions.py

--- pegas_requests_functions.py
+++ pegas_requests_functions.py
@@ -18,8 +18,8 @@ def get_token():
     return json.loads(r.text)['access_token']
 
 
-def get_request(token, num, api, params):
-    if params == {}:
+def get_request(token, num, api, params=None):
+    if params is None:
         r = requests.get(urls[num] + api, headers={'Authorization': f'Bearer {token}'})
     else:
         r = requests.get(urls[num] + api, headers={'Authorization': f'Bearer {token}'}, params=params)
@@ -27,17 +27,24 @@ def get_request(token, num, api, params):
 
 
 def request_configurations_get_all(token):
-    r = get_request(token, 0, '/api/v1/configurations/get-all', {})
+    r = get_request(token, 0, '/api/v1/configurations/get-all')
     return json.loads(r.text)
 
 
 def get_courier_by_id(token, courier_id):
-    r = get_request(token, 1, '/api/v1/couriers/get-courier-by-id/{id}', {'id': courier_id})
-    return r.text
+    r = get_request(token, 1, f'/api/v1/couriers/get-courier-by-id/ {courier_id}', {'id': courier_id})
+    return json.loads(r.text)
+
+
+def get_couriers(token, page_index, page_size, sort_direction=0, search=''):
+    r = get_request(token, 1, '/api/v1/couriers/get-couriers',
+                    {'PageIndex': page_index, 'PageSize': page_size, 'SortDirection': sort_direction, 'Search': search})
+    return json.loads(r.text)
 
 
 if __name__ == "__main__":
     tok = get_token()
-    print(request_configurations_get_all(tok))
-    print(get_courier_by_id(tok, '359afb0c-b870-4610-9233-524db1d5a029'))
+    #print(request_configurations_get_all(tok))
+    #print(get_courier_by_id(tok, '359afb0c-b870-4610-9233-524db1d5a029'))
+    print(get_couriers(tok, 1, 5, 0, 'Максим'))
 
