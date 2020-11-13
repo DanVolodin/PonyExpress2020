0b511edc65b51ffe3c319583a95bca6eead3a63e
Daniil Volodin <volodin.daniil@rambler.ru>
2020-11-13 18:16:32 +0300

    Update pegas_requests_functions.py

--- pegas_requests_functions.py
+++ pegas_requests_functions.py
@@ -4,6 +4,28 @@ import paths as pth
 from paths import urls
 
 
+def ok():
+    with open(pth.logfile, 'a') as logfile:
+        logfile.write('ok\n')
+
+
+def not_ok():
+    with open(pth.logfile, 'a') as logfile:
+        logfile.write('not ok\n')
+
+
+def step(n):
+    with open(pth.logfile, 'a') as logfile:
+        s = '\t Step ' + str(n) + ': '
+        logfile.write(s)
+
+
+def test_num(n):
+    with open(pth.logfile, 'w') as logfile:
+        s = 'Test ' + str(n) + '\n'
+        logfile.write(s)
+
+
 def get_token():
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     data = {'username': pth.correct_login,
@@ -14,8 +36,16 @@ def get_token():
             'client_secret': 'secret'}
     url = "http://srv-pnew-01-test:1001/auth/connect/token"
 
-    r = requests.post(url, data=data, headers=headers)
-    return json.loads(r.text)['access_token']
+    step(0)
+    try:
+        r = requests.post(url, data=data, headers=headers)
+        json.loads(r.text)
+    except:
+        not_ok()
+        return -1
+    else:
+        ok()
+        return json.loads(r.text)['access_token']
 
 
 def get_request(token, num, api, params=None):
@@ -28,23 +58,20 @@ def get_request(token, num, api, params=None):
 
 def request_configurations_get_all(token):
     r = get_request(token, 0, '/api/v1/configurations/get-all')
-    return json.loads(r.text)
+    return r
 
 
 def get_courier_by_id(token, courier_id):
     r = get_request(token, 1, f'/api/v1/couriers/get-courier-by-id/ {courier_id}', {'id': courier_id})
-    return json.loads(r.text)
+    return r
 
 
 def get_couriers(token, page_index, page_size, sort_direction=0, search=''):
     r = get_request(token, 1, '/api/v1/couriers/get-couriers',
                     {'PageIndex': page_index, 'PageSize': page_size, 'SortDirection': sort_direction, 'Search': search})
-    return json.loads(r.text)
+    return r
 
 
 if __name__ == "__main__":
     tok = get_token()
-    #print(request_configurations_get_all(tok))
-    #print(get_courier_by_id(tok, '359afb0c-b870-4610-9233-524db1d5a029'))
-    print(get_couriers(tok, 1, 5, 0, 'Максим'))
 
