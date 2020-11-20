8e101a4838921f3a7a8b4c10eb033c6d64be2cec
Daniil Volodin <volodin.daniil@rambler.ru>
2020-11-20 16:37:36 +0300

    Update r_test_case_2.py

--- r_test_case_2.py
+++ r_test_case_2.py
@@ -5,9 +5,9 @@ import paths as pth
 from paths import urls
 
 import pegas_requests_functions as f
-from functions import ok
-from functions import not_ok
-from functions import step
+from pegas_requests_functions import ok
+from pegas_requests_functions import not_ok
+from pegas_requests_functions import step
 
 
 def test_():
@@ -20,7 +20,6 @@ def test_():
     step(1)
     try:
         r = f.get_couriers(tok, 1, 5, 0, 'Максим')
-
         if r.status_code != 200:
             raise
     except:
@@ -56,3 +55,16 @@ def test_():
 
 if __name__ == "__main__":
     test_()
+
+"""
+Тест-кейс 2: Проверка получения списка пяти курьеров с именами 'Максим'
+    Шаг 0: Получение токена
+        Результат: Код 200
+    Шаг 1: Получение списка данных о курьерах по имени
+        Метод: CourierViewModelIPagedListOperationResult (PageSize = 5, Search = 'Максим')
+        Результат: Код 200
+    Шаг 2: Проверка количества курьеров в списке(['result']['count'])
+        Результат: Количество - 5
+    Шаг 3: Проверка имен(['firstName']) всех курьеров из списка (['result']['items'])
+        Результат: Имя - 'Максим'
+"""
