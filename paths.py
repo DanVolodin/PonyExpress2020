a49d71b74398c79e6494cadb233d1c0c519208fa
Daniil Volodin <volodin.daniil@rambler.ru>
2020-11-20 18:30:55 +0300

    Update paths.py

--- paths.py
+++ paths.py
@@ -18,19 +18,19 @@ wait_time = 0
 
 destination_1 = '1202'
 
-urls = ['http://configurations-backend-edu.pegasus.ponyex.local/',
-        'http://couriers-backend-edu.pegasus.ponyex.local/',
-        'http://delivery-edu.pegasus.ponyex.local/',
-        'http://delivery-backend-edu.pegasus.ponyex.local/',
-        'http://enumerations-backend-edu.pegasus.ponyex.local/',
-        'http://events-edu.pegasus.ponyex.local/',
-        'http://events-backend-edu.pegasus.ponyex.local/',
-        'http://geography-edu.pegasus.ponyex.local/',
-        'http://geography-backend-edu.pegasus.ponyex.local/',
-        'http://localizations-backend-edu.pegasus.ponyex.local/',
-        'http://organization-backend-edu.pegasus.ponyex.local/',
-        'http://warehouses-edu.pegasus.ponyex.local/',
-        'http://warehouses-backend-edu.pegasus.ponyex.local/',
-        'http://waybills-backend-edu.pegasus.ponyex.local/']
+urls = ['http://configurations-backend-edu.pegasus.ponyex.local/', #0
+        'http://couriers-backend-edu.pegasus.ponyex.local/',       #1
+        'http://delivery-edu.pegasus.ponyex.local/',               #2
+        'http://delivery-backend-edu.pegasus.ponyex.local/',       #3
+        'http://enumerations-backend-edu.pegasus.ponyex.local/',   #4
+        'http://events-edu.pegasus.ponyex.local/',                 #5
+        'http://events-backend-edu.pegasus.ponyex.local/',         #6
+        'http://geography-edu.pegasus.ponyex.local/',              #7
+        'http://geography-backend-edu.pegasus.ponyex.local/',      #8
+        'http://localizations-backend-edu.pegasus.ponyex.local/',  #9
+        'http://organization-backend-edu.pegasus.ponyex.local/',   #10
+        'http://warehouses-edu.pegasus.ponyex.local/',             #11
+        'http://warehouses-backend-edu.pegasus.ponyex.local/',     #12
+        'http://waybills-backend-edu.pegasus.ponyex.local/']       #13
 
 swagger_tail = 'api/manual/index.html'
