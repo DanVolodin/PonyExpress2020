30e0efcfa227433fd2345d45794e09c929b7c9fe
Daniil Volodin <volodin.daniil@rambler.ru>
2020-11-27 19:27:13 +0300

    Update pegas_requests_functions.py

--- pegas_requests_functions.py
+++ pegas_requests_functions.py
@@ -50,10 +50,29 @@ def get_token():
 
 
 def get_request(token, num_url, api, params=None):
+    headers = {'Authorization': f'Bearer {token}'}
     if params is None:
-        r = requests.get(urls[num_url] + api, headers={'Authorization': f'Bearer {token}'})
+        r = requests.get(urls[num_url] + api, headers=headers)
     else:
-        r = requests.get(urls[num_url] + api, headers={'Authorization': f'Bearer {token}'}, params=params)
+        r = requests.get(urls[num_url] + api, headers=headers, params=params)
+    return r
+
+
+def post_request(token, num_url, api, data=None):
+    headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'}
+    if data is None:
+        r = requests.post(urls[num_url] + api, headers=headers)
+    else:
+        r = requests.post(urls[num_url] + api, data=json.dumps(data), headers=headers)
+    return r
+
+
+def delete_request(token, num_url, api, params=None):
+    headers = {'Authorization': f'Bearer {token}'}
+    if params is None:
+        r = requests.delete(urls[num_url] + api, headers=headers)
+    else:
+        r = requests.delete(urls[num_url] + api, headers=headers, params=params)
     return r
 
 
@@ -73,11 +92,40 @@ def get_couriers(token, page_index, page_size, sort_direction=0, search=''):
     return r
 
 
-def get_sending_all_condition_parameters(token):
+def get_sending_condition_parameters(token):
     r = get_request(token, 12, '/api/v1/condition-parameters/get-all')
+    return r
+
+
+def get_groups_list(token):
+    r = get_request(token, 14, pth.url_get_groups_list)
+    return r
+
+
+def check_group_existence(token, group_name):
+    try:
+        r = get_groups_list(token)
+        if r.status_code != 200:
+            raise
+    except:
+        return False
+    r = json.loads(r.text)
+    for group in r['result']:
+        if group['displayName'] == group_name:
+            return group['id']
+    return False
+
+
+def create_group(token, group_name):
+    r = post_request(token, 14, pth.url_group_post, {'displayName': group_name})
+    return r
+
+
+def delete_group(token, group_id):
+    r = delete_request(token, 14, pth.url_group_delete, {'id': group_id})
     return r
 
 
 if __name__ == "__main__":
     tok = get_token()
-
+    print(check_group_existence(tok, 'Бета-тестеры'))
