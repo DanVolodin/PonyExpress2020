39ce493797f4fef4baa73105796fd834a137c58d
Daniil Volodin <volodin.daniil@rambler.ru>
2021-02-12 19:22:36 +0300

    Update r_test_case_12.py

--- r_test_case_12.py
+++ r_test_case_12.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import allure
 
 import paths as pth
 from paths import urls
@@ -11,59 +12,52 @@ from pegas_requests_functions import step
 
 
 def test_():
-    f.test_num(9)
-
     tok = f.get_token()
-    if tok == -1:
-        return
+    with allure.step('step 0: '):
+        assert tok != -1, 'get_token fail'
 
-    step(1)
-    try:
-        r = f.create_event_blocks_71(tok)
-        if r.status_code != 200:
-            raise
-    except:
-        not_ok()
-        return
-    else:
-        ok()
+    r = f.create_event_blocks_71(tok)
+    with allure.step('step 1: '):
+        assert r.status_code == 200, 'create_event_blocks_71 fail'
 
     r = json.loads(r.text)
+    block_number = r['result']['number']
+    with allure.step('step 2: '):
+        assert block_number[0:3] == 'MOW', 'invalid block_id'
+
     block_id = r['result']['id']
-
-    step(2)
-    try:
-        if not block_id:
-            raise
-    except:
-        not_ok()
-        return
-    else:
-        ok()
-
-    step(3)
-    try:
-        r = f.create_event_blocks_71_object(tok, block_id, pth.correct_71_block_num)
-        if r.status_code != 200:
-            raise
-    except:
-        not_ok()
-        return
-    else:
-        ok()
+    r = f.create_event_blocks_71_object(tok, block_id, pth.correct_71_block_num)
+    with allure.step('step 3'):
+        assert r.status_code == 200, 'create_event_blocks_71_object fail'
 
     r = json.loads(r.text)
-    object_status = r['ok']
+    post_ok = r['ok']
+    with allure.step('step 4: '):
+        assert post_ok, 'object not created'
 
-    step(4)
-    try:
-        if not object_status:
-            raise
-    except:
-        not_ok()
-        return
-    else:
-        ok()
+    object_id = r['result']['id']
+    r = f.get_event_blocks_71_object(tok, block_id, object_id)
+    with allure.step('step 5'):
+        assert r.status_code == 200, 'get_event_blocks_71_object fail'
+
+    r = json.loads(r.text)
+    find_ok = r['ok']
+    with allure.step('step 6: '):
+        assert find_ok, 'object not found'
+
+    r = f.delete_event_blocks_71_object(tok, block_id, object_id)
+    with allure.step('step 7: '):
+        assert r.status_code == 200, 'delete_event_blocks_71_object fail'
+    print(json.loads(r.text))
+
+    r = f.get_event_blocks_71_object(tok, block_id, object_id)
+    with allure.step('step 5'):
+        assert r.status_code == 200, 'get_event_blocks_71_object fail'
+
+    r = json.loads(r.text)
+    find_ok = r['ok']
+    with allure.step('step 6: '):
+        assert not find_ok, 'object found'
 
 
 if __name__ == "__main__":
@@ -75,10 +69,17 @@ if __name__ == "__main__":
         Результат: Код 200
     Шаг 1: Создание блока событий "71. Прибыл на склад (без сортировки)" без курьера
         Результат: Код 200
-    Шаг 2: Проверка, что полученный id блока непустой
-        Результат: id блока непустой
+    Шаг 2: Проверка, что полученный номер блока корректен
+        Результат: number блока начинается с 'MOW'
     Шаг 3: Добавление объекта с корректным номером "11-1111-1111"
         Результат: Код 200
-    Шаг 4: Проверка статуса ответа
-        Результат: Статус r['ok'] = true
+    Шаг 4: Проверка, что нет ошибки добавления
+        Результат: r['ok'] = true
+    Шаг 5: Запрос объекта по id
+        Результат: Код 200
+    Шаг 6: Проверка, что объект найден
+        Результат: r['ok'] = true
+    Шаг 7: Удаление объекта по id
+        Результат: Код 200
+    Шаг 8: 
 """
