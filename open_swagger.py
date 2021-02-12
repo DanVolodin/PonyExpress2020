f9667059f2436b465a8c569211014cc261e843df
Daniil Volodin <volodin.daniil@rambler.ru>
2021-02-12 14:14:43 +0300

    Update open_swagger.py

--- open_swagger.py
+++ open_swagger.py
@@ -5,7 +5,13 @@ import paths as pth
 
 
 def open_page(num):
-    driver = webdriver.Safari()
+    driver = 0
+    if pth.browser == 'Safari':
+        driver = webdriver.Safari()
+    if pth.browser == 'Firefox':
+        driver = webdriver.Firefox(executable_path=pth.driver_path)
+    if pth.browser == 'Chrome':
+        driver = webdriver.Chrome(executable_path=pth.driver_path)
     driver.get(pth.urls[num] + pth.swagger_tail)
     return driver
 
