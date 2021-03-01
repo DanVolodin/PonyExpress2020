f435871dbf0cafe5092e64809c8530a8e2aa4554
Daniil <volodin.daniil@rambler.ru>
2021-03-01 13:20:17 +0300

    Create test_case_8_r.py

---
+++ test_case_8_r.py
@@ -0,0 +1,37 @@
+import requests
+import json
+import allure
+import pytest
+
+import paths as pth
+
+import pegas_requests_functions as f
+
+
+def test_():
+    tok = f.get_token()
+    with allure.step('step 0: '):
+        assert tok != -1, 'get_token fail'
+
+    r = f.create_event_blocks_71(tok)
+    with allure.step('step 1: '):
+        assert r.status_code == 200, 'create_event_blocks_71 fail'
+
+    r = json.loads(r.text)
+    block_id = r['result']['number']
+    with allure.step('step 2: '):
+        assert block_id[0:3] == 'MOW', 'invalid block_id'
+
+
+if __name__ == "__main__":
+    test_()
+
+"""
+Тест-кейс 8: Создание блока событий "71. Прибыл на склад (без сортировки)" без курьера
+    Шаг 0: Получение токена
+        Результат: Код 200
+    Шаг 1: Создание блока событий "71. Прибыл на склад (без сортировки)" без курьера
+        Результат: Код 200
+    Шаг 2: Проверка, что номер полученного блока корректен
+        Результат: number блока начинается с 'MOW'
+"""
