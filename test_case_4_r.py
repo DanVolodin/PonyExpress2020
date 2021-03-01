54f87ec9bfc0a98200b85754bc48fc3bdd37d3cd
Daniil <volodin.daniil@rambler.ru>
2021-03-01 15:05:34 +0300

    Update test_case_4_r.py

--- test_case_4_r.py
+++ test_case_4_r.py
@@ -9,23 +9,23 @@ import pegas_requests_functions as f
 
 
 def test_():
-    tok = f.get_token()
     with allure.step('step 0: '):
+        tok = f.get_token()
         assert tok != -1, 'get_token fail'
 
     with allure.step('step 1: '):
         assert not f.check_group_existence(tok, pth.random_name), 'group already exists'
 
-    r = f.create_group(tok, pth.random_name)
     with allure.step('step 2: '):
+        r = f.create_group(tok, pth.random_name)
         assert r.status_code == 200, 'create_group fail'
 
     with allure.step('step 3: '):
         assert f.check_group_existence(tok, pth.random_name), 'group not created'
 
-    group_id = f.check_group_existence(tok, pth.random_name)
-    r = f.delete_group(tok, group_id)
     with allure.step('step 4: '):
+        group_id = f.check_group_existence(tok, pth.random_name)
+        r = f.delete_group(tok, group_id)
         assert r.status_code == 200, 'delete_group fail'
 
     with allure.step('step 5: '):
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     test_()
 
 """
-Тест-кейс 4: Добавление группы пользователей
+Тест-кейс 4: Добавление и удаление группы пользователей
     Шаг 0: Получение токена
         Результат: Код 200
     Шаг 1: Проверка, что создаваемой группы пользователей нет в списке
